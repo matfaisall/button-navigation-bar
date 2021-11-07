4aa97c400f4be7e4383f7571671c91b53fef36e8
Muhammad Faisal <muh.faisal572@gmail.com>
2021-11-07 08:02:50 +0700

    merubah background header apabila di scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,11 @@ function scrollActive() {
 	});
 }
 window.addEventListener("scroll", scrollActive);
+
+/* ============ SCROLL SECTIONS ACTIVE LINK ============= */
+function scrollHeader(){
+   const header = document.getElementById('header')
+   // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+   if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
